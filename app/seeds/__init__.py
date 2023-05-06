7c005a17a245500291b6a33cdd5d56fd958b7435
Your Name <Your Email>
2023-05-06 15:50:57 -0500

    init.py corrections

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,5 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
-from .albums import seed_albums, undo_albums
 
 from app.models.db import db, environment, SCHEMA
 
@@ -18,9 +17,7 @@ def seed():
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
         undo_users()
-        undo_albums()
     seed_users()
-    seed_albums()
     # Add other seed functions here
 
 
@@ -28,5 +25,4 @@ def seed():
 @seed_commands.command('undo')
 def undo():
     undo_users()
-    undo_albums()
     # Add other undo functions here
