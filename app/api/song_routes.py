6735620e88f21d1a67293a0260baf9540d4686e5
Joshua <joshdante@hotmail.com>
2023-05-07 16:22:42 -0400

    adding backend song routes

--- app/api/song_routes.py
+++ app/api/song_routes.py
@@ -2,4 +2,12 @@ from flask import Blueprint
 from app.models import Song
 
 
+song_routes = Blueprint('songs', __name__)
 
+
+@song_routes.route('/')
+def songs():
+    """Query for all songs and return them in a list of dictionaries"""
+    songs = Song.query.all()
+    print('adsfasdfadfadfasfd',songs)
+    return songs.to_dict()
