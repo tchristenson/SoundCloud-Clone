d094bd547ab3bea494067afbc8874e41f219d26c
Joshua <joshdante@hotmail.com>
2023-05-06 21:09:01 -0400

    rendered user data

--- react-app/src/components/UserProfilePage/index.js
+++ react-app/src/components/UserProfilePage/index.js
@@ -1,31 +1,40 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
-import { useParams } from 'react-router-dom';
-import { useEffect } from 'react';
-import { getOneUserThunk } from '../../store/users';
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { getOneUserThunk } from "../../store/users";
 
 function UserProfilePage() {
-    const dispatch = useDispatch();
-    //const profileUser = useSelector((state));
-    const { userId } = useParams();
+  const dispatch = useDispatch();
+  //const profileUser = useSelector((state));
+  const { userId } = useParams();
 
-    useEffect(() => {
-        dispatch(getOneUserThunk(userId))
-    }, [userId, dispatch])
+  useEffect(() => {
+    dispatch(getOneUserThunk(userId));
+  }, [userId, dispatch]);
 
-    const user = useSelector((state) => state.users[userId]);
+  const user = useSelector((state) => state.users[userId]);
 
-    if (!user) {
-        return null
-    }
+  if (!user) {
+    return null;
+  }
 
-    return (
-        <>
-            <h1>Testing</h1>
-            <p>{user.alias}</p>
-        </>
-    )
+  return (
+    <>
+      <h1>Testing</h1>
+
+      <div>
+        {" "}
+        <div>{user.profileImage} </div>
+        <p>{user.username}</p>
+        <div>
+          {user.firstName} {user.lastName}
+        </div>
+      </div>
+      <div></div>
+    </>
+  );
 }
 
 export default UserProfilePage;
