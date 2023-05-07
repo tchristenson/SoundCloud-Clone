f941be73e85421403d68ff8896e819641c810136
Joshua <joshdante@hotmail.com>
2023-05-06 21:25:15 -0400

    user foreignkey import

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,6 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.schema import ForeignKey
 from flask_login import UserMixin
 # from .style import Style
 # from .song import Song
