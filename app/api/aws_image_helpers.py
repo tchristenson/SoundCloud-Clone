62b208b9bd7dea2e235292dc820ff5cb9b2bfdfe
Your Name <Your Email>
2023-05-08 13:11:59 -0500

    aws image prepwork

--- app/api/aws_image_helpers.py
+++ app/api/aws_image_helpers.py
@@ -3,22 +3,22 @@ import botocore
 import os
 import uuid
 
-BUCKET_NAME = os.environ.get("S3_BUCKET")
+BUCKET_NAME = os.environ.get("S3_IMAGE_BUCKET")
 S3_LOCATION = f"http://{BUCKET_NAME}.s3.amazonaws.com/"
-ALLOWED_SONG_EXTENSIONS = {"flac", "wav", "alac", "aiff", "mp3", "mp4", "mp2", "ogg", "aac", "amr", "wma"}
+ALLOWED_IMAGE_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
 
 s3 = boto3.client(
    "s3",
-   aws_access_key_id=os.environ.get("S3_KEY"),
-   aws_secret_access_key=os.environ.get("S3_SECRET")
+   aws_access_key_id=os.environ.get("S3_IMAGE_KEY"),
+   aws_secret_access_key=os.environ.get("S3_IMAGE_SECRET")
 )
 
-def get_unique_filename(filename):
+def get_unique_image_filename(filename):
     ext = filename.rsplit(".", 1)[1].lower()
     unique_filename = uuid.uuid4().hex
     return f"{unique_filename}.{ext}"
 
-def upload_file_to_s3(file, acl="public-read"):
+def upload_image_file_to_s3(file, acl="public-read"):
     try:
         s3.upload_fileobj(
             file,
@@ -35,8 +35,8 @@ def upload_file_to_s3(file, acl="public-read"):
 
     return {"url": f"{S3_LOCATION}{file.filename}"}
 
-def remove_file_from_s3(url):
-    # AWS needs the song file name, not the URL,
+def remove_image_file_from_s3(url):
+    # AWS needs the IMAGE file name, not the URL,
     # so we split that out of the URL
     key = url.rsplit("/", 1)[1]
     print(key)
