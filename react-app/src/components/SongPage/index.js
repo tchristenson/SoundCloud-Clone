ef045e51b196ce670c511d59a136462a5faff499
anwersaad0 <anwer.saad0@gmail.com>
2023-05-12 01:15:26 -0700

    added css to default songs, applied navlink to user songs

--- react-app/src/components/SongPage/index.js
+++ react-app/src/components/SongPage/index.js
@@ -4,6 +4,7 @@ import { NavLink, Redirect } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import { getAllSongsThunk } from "../../store/songs";
+import './SongPage.css'
 
 function SongPage() {
   const dispatch = useDispatch();
@@ -22,12 +23,19 @@ function SongPage() {
     <div id="songPage">
       {songs?.map(({name,albumId, styleId, ownerId, runtime, coverImage, content, id})=>(
         <NavLink to={`/songs/${id}`} key={id}>
-          <div className="playlogo"></div>
-          <div>Title: {name}</div>
-          <div>user name ? (owner id):{ownerId} , style: {styleId}</div>
-          <div>album name? album id: {albumId}</div>
-          <div>wav thing</div>
-          <img src={coverImage}/>
+          <div className="song-div">
+            <div className="song-picture-div">
+              <img className="song-picture" src={coverImage}/>
+            </div>
+
+            <div>
+              <div className="playlogo"></div>
+              <div className="song-name">Title: {name}</div>
+              <div>user name ? (owner id):{ownerId} , style: {styleId}</div>
+              <div>album name? album id: {albumId}</div>
+              <div>wav thing</div>
+            </div>
+          </div>
         </NavLink>
       ))}
     </div>
--- react-app/src/components/UsersSongPage/index.js
+++ react-app/src/components/UsersSongPage/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
+import { NavLink, Redirect } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import { getCurrentUsersSongsThunk } from "../../store/songs";
@@ -17,6 +17,7 @@ function UsersSongsPage() {
     const songs = useSelector((state) => Object.values(state.songs));
     const sessionUser = useSelector(state => state.session.user);
     console.log(sessionUser)
+    console.log(songs[0]);
 
     if(!songs) {
         return <h1>no current users songs found</h1>
@@ -29,21 +30,23 @@ function UsersSongsPage() {
         //     <h4>song name above?</h4>
         // </div>
         <div id="usersSongPage">
-          {/* <h1 className="user-songs-title">{sessionUser.username}'s Songs</h1> */}
+          <h1 className="user-songs-title">{sessionUser.username}'s Songs</h1>
         {songs?.map(({name,albumId, styleId, ownerId, runtime, coverImage, content, id})=>(
-          <div className="song-div" key={id}>
-            <div className="song-picture-div">
-              <img className="song-picture" src={coverImage}/>
+          <NavLink to={`/songs/${id}`} key={id}>
+            <div className="song-div" key={id}>
+              <div className="song-picture-div">
+                <img className="song-picture" src={coverImage}/>
+              </div>
+              
+              <div>
+                <div className="playlogo"></div>
+                <div className="song-name">{name}</div>
+                <div>By {sessionUser.username} (owner id):{ownerId} , style: {styleId}</div>
+                <div>album name? album id: {albumId}</div>
+                <div>wav thing</div>
+              </div>
             </div>
-            
-            <div>
-              <div className="playlogo"></div>
-              <div className="song-name">{name}</div>
-              <div>By {sessionUser.username} (owner id):{ownerId} , style: {styleId}</div>
-              <div>album name? album id: {albumId}</div>
-              <div>wav thing</div>
-            </div>
-          </div>
+          </NavLink>
         ))}
       </div>
     )
