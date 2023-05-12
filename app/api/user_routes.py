83da2ea2941207445eea57a0c69ea11d054585a0
Tommy Christenson <tchristenson21@gmail.com>
2023-05-12 18:09:20 -0500

    fixed spaces

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -6,7 +6,6 @@ user_routes = Blueprint('users', __name__)
 
 
 @user_routes.route('/')
-
 def users():
     """
     Query for all users and returns them in a list of user dictionaries
@@ -16,7 +15,6 @@ def users():
 
 
 @user_routes.route('/<int:id>')
-
 def user(id):
     """
     Query for a user by id and returns that user in a dictionary
