afcd2ede8b3009cf4e64dbd080679ee6217301fc
anwersaad0 <anwer.saad0@gmail.com>
2023-05-13 20:17:37 -0700

    added styling to delete modals

--- react-app/src/components/AlbumDeleteModal/index.js
+++ react-app/src/components/AlbumDeleteModal/index.js
@@ -2,6 +2,7 @@ import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { deleteAlbumThunk } from "../../store/albums";
 import { useModal } from "../../context/Modal";
+import './AlbumDeleteModal.css'
 
 function AlbumDeleteModal({albumId}) {
     const dispatch = useDispatch();
@@ -22,11 +23,11 @@ function AlbumDeleteModal({albumId}) {
     }
 
     return (
-        <div>
-            <h1>Delete Album?</h1>
+        <div className="delete-album-div">
+            <h1>Delete This Album?</h1>
             <form onSubmit={handleDelete}>
-                <button type="submit">Yes, delete the album</button>
-                <button onClick={closeModal}>No, keep this album</button>
+                <button className="confirm-album-delete" type="submit">Yes, delete the album</button>
+                <button className="decline-album-delete" onClick={closeModal}>No, keep this album</button>
             </form>
         </div>
     )
